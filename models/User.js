19cafc73ccea428fd27b6de74c5cdc01a509cc6e
mblaul <mattblaul@gmail.com>
2018-06-03 23:30:26 -0400

    Created user model.

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create schema
+
+const UserSchema = new Schema({
+	name: {
+		type: String,
+		required: true
+	},
+	email: {
+		type: String,
+		required: true
+	},
+	password: {
+		type: String,
+		required: true
+	},
+	avater: {
+		type: String,
+		required: true
+	},
+	date: {
+		type: String,
+		dafault: Date.now
+	}
+});
+
+module.exports = User = mongoose.model("users", UserSchema);
