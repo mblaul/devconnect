0088debec381adcda14ab09ffb5fbdd8eef7ed12
mblaul <mattblaul@gmail.com>
2018-06-11 19:40:02 -0400

    Started working on profiles.

--- models/User.js
+++ models/User.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // Create schema
-
 const UserSchema = new Schema({
 	name: {
 		type: String,
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,41 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
 
-// @route   GET api/profiles/test
+//Load Models
+const Profile = require("../../models/Profle");
+const User = require("../../models/User");
+
+// @route   GET api/profile/test
 // @desc    Test profiles route
 // @access  Public
 router.get("/test", (req, res) => {
 	res.json({ message: "Test comprete!" });
 });
+
+// @route   GET api/profile/
+// @desc    Check current user's porfile
+// @access  Private
+router.get(
+	"/",
+	passport.authenticate("jwt", { session: false }),
+	(req, res) => {
+		const errors = {};
+
+		Profile.findOne({ user: req.user.id })
+			.then(profile => {
+				console.log("in here!");
+				console.log(profile);
+				if (!profile) {
+					console.log("in here2!");
+					errors.noprofile = "There is no profile for this user";
+					return res.status(404).json(errors);
+				}
+				res.json(profile);
+			})
+			.catch(err => res.status(404).json(err));
+	}
+);
 
 module.exports = router;
