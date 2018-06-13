6c94808bf84b18fc321b86e4c3358feac8f40a1e
mblaul <mattblaul@gmail.com>
2018-06-13 19:46:27 -0400

    Can now delete individual experience records from profiles.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -230,4 +230,29 @@ router.post(
 	}
 );
 
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    Delete experience from profile
+// @access  Private
+
+router.delete(
+	"/experience/:exp_id",
+	passport.authenticate("jwt", { session: false }),
+	(req, res) => {
+		Profile.findOne({ user: req.user.id })
+			.then(profile => {
+				//Get remove index
+
+				const removeIndex = profile.experience
+					.map(item => item.id)
+					.indexOf(req.params.exp_id);
+
+				// Splice out of array
+				profile.experience.splice(removeIndex, 1);
+
+				profile.save().then(profile => res.json(profile));
+			})
+			.catch(err => res.status(404).json(err));
+	}
+);
+
 module.exports = router;
