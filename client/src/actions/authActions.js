6d925e29585b8e217d94f838e0930d252aa69c07
mblaul <mattblaul@gmail.com>
2018-07-07 23:57:23 -0400

    Added basic logout functionality

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -48,3 +48,13 @@ export const setCurrentUser = decoded => {
 		payload: decoded
 	};
 };
+
+// Log user out
+export const logoutUser = () => dispatch => {
+	// Remove token from local storage
+	localStorage.removeItem("jwtToken");
+	// Remove auth header
+	setAuthToken(false);
+	// Set current user to empty object which will set isAuthenticated to false also
+	dispatch(setCurrentUser({}));
+};
