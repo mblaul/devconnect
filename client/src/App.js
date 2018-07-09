969a57bd2c5587fcca47d31a0c9fbe6e2c0245f1
mblaul <mattblaul@gmail.com>
2018-07-08 22:40:24 -0400

    Started working on profiles. Profile is now stored in Store upon login. Placeholder objects for now.

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,14 @@ import store from "./store";
 import jwtDecode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
 import { setCurrentUser, logoutUser } from "./actions/authActions";
+import { clearCurrentProfile } from "./actions/profileActions";
 
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Footer from "./components/layout/Footer";
 import Register from "./components/authentication/Register";
 import Login from "./components/authentication/Login";
+import Dashboard from "./components/dashboard/Dashboard";
 
 import "./App.css";
 
@@ -28,7 +30,7 @@ if (localStorage.jwtToken) {
 	if (decoded.exp < currentTime) {
 		// Logout the user
 		store.dispatch(logoutUser());
-		// TODO Clear the current profile
+		store.dispatch(clearCurrentProfile());
 		// Redirect to login
 		window.location.href = "./login";
 	}
@@ -45,6 +47,7 @@ class App extends Component {
 						<div className="container">
 							<Route exact path="/register" component={Register} />
 							<Route exact path="/login" component={Login} />
+							<Route exact path="/dashboard" component={Dashboard} />
 						</div>
 						<Footer />
 					</div>
---
+++ client/src/actions/profileActions.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+import {
+	GET_PROFILE,
+	PROFILE_LOADING,
+	CLEAR_CURRENT_PROFILE,
+	GET_ERRORS
+} from "./types";
+
+// Get current profile
+
+export const getCurrentProfile = () => dispatch => {
+	dispatch(setProfileLoading());
+	axios
+		.get("/api/profile")
+		.then(result =>
+			dispatch({
+				type: GET_PROFILE,
+				payload: result.data
+			})
+		)
+		.catch(err =>
+			dispatch({
+				type: GET_PROFILE, // We don't use GET_ERRORS here because it's okay if they don't have a profile
+				payload: {}
+			})
+		);
+};
+
+// Profile loading
+export const setProfileLoading = () => {
+	return {
+		type: PROFILE_LOADING
+	};
+};
+
+// Clear profile
+export const clearCurrentProfile = () => {
+	return {
+		type: CLEAR_CURRENT_PROFILE
+	};
+};
