18aeef5e02392dce7c9124472f2ca5716599a6fb
mblaul <mattblaul@gmail.com>
2018-07-13 23:44:07 -0400

    Worked on form for create profile some more.

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -26,9 +26,75 @@ class CreateProfile extends Component {
 			instagram: "",
 			errors: {}
 		};
+
+		this.onChange = this.onChange.bind(this);
+		this.onSubmit = this.onSubmit.bind(this);
+	}
+
+	onSubmit(e) {
+		e.preventDefault();
+		console.log("submit");
+	}
+
+	onChange(e) {
+		this.setState({ [e.target.name]: e.target.value });
 	}
 
 	render() {
+		const { errors, displaySocialInputs } = this.state;
+
+		let socialInputs;
+
+		if (displaySocialInputs) {
+			socialInputs = (
+				<div>
+					<InputGroup
+						placeholder="Twitter Profile URL"
+						name="twitter"
+						icon="fab fa-twitter"
+						value={this.state.twitter}
+						onChange={this.onChange}
+						error={errors.twitter}
+					/>
+					<InputGroup
+						placeholder="Facebook Profile URL"
+						name="facebook"
+						icon="fab fa-facebook"
+						value={this.state.facebook}
+						onChange={this.onChange}
+						error={errors.facebook}
+					/>
+					<InputGroup
+						placeholder="Youtube Profile URL"
+						name="youtube"
+						icon="fab fa-youtube"
+						value={this.state.youtube}
+						onChange={this.onChange}
+						error={errors.youtube}
+					/>
+					<InputGroup
+						placeholder="Instagram Profile URL"
+						name="Instagram"
+						icon="fab fa-Instagram"
+						value={this.state.Instagram}
+						onChange={this.onChange}
+						error={errors.Instagram}
+					/>
+				</div>
+			);
+		}
+		// Select options for professional status
+		const options = [
+			{ label: "* Select Professional Status", value: 0 },
+			{ label: "Developer", value: "Developer" },
+			{ label: "Junior Developer", value: "Junior Developer" },
+			{ label: "Senior Developer", value: "Senior Developer" },
+			{ label: "Manager", value: "Manager" },
+			{ label: "Student", value: "Student" },
+			{ label: "Instructor", value: "Instructor" },
+			{ label: "Intern", value: "Intern" },
+			{ label: "Other", value: "Other" }
+		];
 		return (
 			<div className="create-profile">
 				<div className="container">
@@ -39,6 +105,92 @@ class CreateProfile extends Component {
 								Let's collect some information!
 							</p>
 							<small className="d-block pb-3"> * = required fields</small>
+							<form onSubmit={this.onSubmit}>
+								<TextFieldGroup
+									placeholder="* Profile Handle"
+									name="handle"
+									value={this.state.handle}
+									onChange={this.onChange}
+									error={errors.handle}
+									info="A unique name for your profile URL. Your full name, company name, nickname, ETC."
+								/>
+								<SelectListGroup
+									placeholder="Status"
+									name="status"
+									value={this.state.status}
+									onChange={this.onChange}
+									options={options}
+									error={errors.status}
+									info="Please select your experience level"
+								/>
+								<TextFieldGroup
+									placeholder="Company"
+									name="company"
+									value={this.state.company}
+									onChange={this.onChange}
+									error={errors.company}
+									info="Could be your own company or one you work for"
+								/>
+								<TextFieldGroup
+									placeholder="Website"
+									name="website"
+									value={this.state.website}
+									onChange={this.onChange}
+									error={errors.website}
+									info="Your personal or company website"
+								/>
+								<TextFieldGroup
+									placeholder="Location"
+									name="location"
+									value={this.state.location}
+									onChange={this.onChange}
+									error={errors.location}
+									info="City or city and state suggested (e.g. Boston, MA)"
+								/>
+								<TextFieldGroup
+									placeholder="* Skills"
+									name="skills"
+									value={this.state.skills}
+									onChange={this.onChange}
+									error={errors.skills}
+									info="Please use comma separated values (e.g. HTML, CSS, Javascript, PHP)"
+								/>
+								<TextFieldGroup
+									placeholder="GitHub Username"
+									name="githubusername"
+									value={this.state.githubusername}
+									onChange={this.onChange}
+									error={errors.githubusername}
+									info="If you want your latest repos and a GitHub link please include your username"
+								/>
+								<TextAreaFieldGroup
+									placeholder="Short Bio"
+									name="bio"
+									value={this.state.bio}
+									onChange={this.onChange}
+									error={errors.bio}
+									info="Please use comma separated values (e.g. HTML, CSS, Javascript, PHP)"
+								/>
+								<div className="mb-3">
+									<button
+										onClick={() => {
+											this.setState(prevState => ({
+												displaySocialInputs: !prevState.displaySocialInputs
+											}));
+										}}
+										className="btn btn-light"
+									>
+										Add Social Network Links
+									</button>
+									<span className="text muted">Optional</span>
+								</div>
+								{socialInputs}
+								<input
+									type="submit"
+									value="submit"
+									className="btn btn-info btn-block mt-4"
+								/>
+							</form>
 						</div>
 					</div>
 				</div>
