7cd27c02ccf4dfc22e0ffcd836293bc3c64a4f6a
mblaul <mattblaul@gmail.com>
2018-07-23 21:36:13 -0400

    Implemented ProfileActions properly

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -34,19 +34,6 @@ class Dashboard extends Component {
 							Welcome{" "}
 							<Link to={`/profile/${profile.handle}`}> {user.name} </Link>
 						</p>
-						<div className="btn-group mb-4" role="group">
-							<a href="./edit-profile" className="btn btn-light">
-								<i className="fas fa-user-circle text-info mr-1" /> Edit Profile
-							</a>
-							<a href="/add-experience" className="btn btn-light">
-								<i className="fab fa-black-tie text-info mr-1" />
-								Add Experience
-							</a>
-							<a href="/add-education" className="btn btn-light">
-								<i className="fas fa-graduation-cap text-info mr-1" />
-								Add Education
-							</a>
-						</div>
 						<ProfileActions />
 						<Experience experience={profile.experience} />
 						<Education education={profile.education} />
