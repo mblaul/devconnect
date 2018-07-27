d90a753e23e6bd4d3d87ec42817668e4383c42a6
mblaul <mattblaul@gmail.com>
2018-07-26 21:22:01 -0400

    Added githubusername section in user profile.

--- client/src/App.js
+++ client/src/App.js
@@ -21,6 +21,7 @@ import AddExperience from "./components/add-credentials/AddExperience";
 import AddEducation from "./components/add-credentials/AddEducation";
 import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
+import NotFound from "./components/not-found/NotFound";
 
 import "./App.css";
 
@@ -89,6 +90,7 @@ class App extends Component {
 									component={AddEducation}
 								/>
 							</Switch>
+							<Route exact path="/notfound" component={NotFound} />
 						</div>
 						<Footer />
 					</div>
