499bfe7aa6a9632350fc01fe67edfc129220adbd
mblaul <mattblaul@gmail.com>
2018-08-02 22:11:08 -0400

    Added delete button for posts

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,11 +3,15 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import classnames from "classnames";
 import { Link } from "react-router-dom";
+import { pseudoRandomBytes } from "crypto";
 
 class PostItem extends Component {
+	onDeleteClick(id) {
+		console.log(id);
+	}
+
 	render() {
 		const { post, auth } = this.props;
-
 		return (
 			<div className="card card-body mb-3">
 				<div className="row">
@@ -15,33 +19,33 @@ class PostItem extends Component {
 						<a href="profile.html">
 							<img
 								className="rounded-circle d-none d-md-block"
-								src={post.avatar}
+								src={`https://` + post.avatar}
 								alt=""
 							/>
 						</a>
 						<br />
-						<p className="text-center">John Doe</p>
+						<p className="text-center">{post.name}</p>
 					</div>
 					<div className="col-md-10">
-						<p className="lead">
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
-							possimus corporis sunt necessitatibus! Minus nesciunt soluta
-							suscipit nobis. Amet accusamus distinctio cupiditate blanditiis
-							dolor? Illo perferendis eveniet cum cupiditate aliquam?
-						</p>
+						<p className="lead">{post.text}</p>
 						<button type="button" className="btn btn-light mr-1">
 							<i className="text-info fas fa-thumbs-up" />
-							<span className="badge badge-light">4</span>
+							<span className="badge badge-light">{post.likes.length}</span>
 						</button>
 						<button type="button" className="btn btn-light mr-1">
 							<i className="text-secondary fas fa-thumbs-down" />
 						</button>
-						<a href="post.html" className="btn btn-info mr-1">
+						<Link to={`/post/${post._id}`} className="btn btn-info mr-1">
 							Comments
-						</a>
-						<button type="button" className="btn btn-danger mr-1">
-							<i className="fas fa-times" />
-						</button>
+						</Link>
+						{post.user === auth.user.id ? (
+							<button
+								onClick={this.onDeleteClick.bind(this, post._id)}
+								className="btn btn-danger mr-1"
+							>
+								<i className="fas fa-times" />
+							</button>
+						) : null}
 					</div>
 				</div>
 			</div>
@@ -49,7 +53,7 @@ class PostItem extends Component {
 	}
 }
 
-PostItem.PropTypes = {
+PostItem.propTypes = {
 	post: PropTypes.object.isRequired,
 	auth: PropTypes.object.isRequired
 };
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -54,7 +54,7 @@ router.post(
 		const newPost = new Post({
 			text: req.body.text,
 			name: req.body.name,
-			avatar: req.body.name,
+			avatar: req.body.avatar,
 			user: req.user.id
 		});
 
