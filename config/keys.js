243f33414c91e2a8006a76b6a4c6f8ab867fbe4b
mblaul <mattblaul@gmail.com>
2018-08-04 19:51:46 -0400

    Readying app for deployment

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-	mongoURI:
-		"mongodb://admin:superadminboi1@ds115701.mlab.com:15701/devconnector",
-	secretOrKey: "biglittlesecret"
-};
+if (process.env.NODE_ENV === "production") {
+	module.exports = require("./keys_prod");
+} else {
+	module.exports = require("./keys_dev");
+}
